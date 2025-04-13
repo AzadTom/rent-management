618147ef545d60384fb0ffd44cd5415af9f0d706
azadtom <kumarazad2917@gmail.com>
2025-04-13 22:57:06 +0530

    added chnages!

--- app/userinfo/[username]/page.tsx
+++ app/userinfo/[username]/page.tsx
@@ -2,7 +2,7 @@ import UserInfo from "@/components/UserInfo/UserInfo";
 import { getAllDataByUser } from "@/services/api";
 
 type Props = {
-  params: Promise<{ username: string }>;
+  params: Promise<{username:string}>;
 };
 
 const page = async ({ params }: Props) => {
